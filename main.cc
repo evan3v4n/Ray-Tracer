ec332fd80ef8dd619022dc8fc7c0514f73ef1070
evan3v4n <evan.scott2050@Gmail.com>
2023-11-02 16:53:03 -0400

    ray-sphere interaction and hittable classes

--- main.cc
+++ main.cc
@@ -1,21 +1,21 @@
+#include "rtweekend.h"
+
 #include "color.h"
 #include "vec3.h"
 #include "ray.h"
 
-#include <iostream>
-// Making a sphere
-bool hit_sphere(const point3& center, double radius, const ray& r) {
-    vec3 oc = r.origin() - center;
-    auto a = dot(r.direction(), r.direction());
-    auto b = 2.0 * dot(oc, r.direction());
-    auto c = dot(oc, oc) - radius*radius;
-    auto discriminant = b*b - 4*a*c;
-    return (discriminant >= 0);
-}
+#include "hittable.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
-color ray_color(const ray& r) {
-    if (hit_sphere(point3(0,0,-1), 0.5, r))
-        return color(1,0,0);
+
+#include <iostream>
+
+color ray_color(const ray& r, const hittable& world) {
+    hit_record rec;
+    if (world.hit(r, 0, infinity, rec)) {
+        return 0.5 * (rec.normal + color(1,1,1));
+    }
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
     return (1.0-a)*color(1.0,1.0,1.0) + a*color(0.5,0.7,1.0);
@@ -32,8 +32,11 @@ int main() {
     int image_height = static_cast<int>(image_width / aspect_ratio);
     image_height = (image_height < 1 ) ? 1 : image_height;
 
-    // Viewport widths less than one are okay since they are real valued.
-    // auto viewport_height = 2.0;
+    // World
+    hittable_list world;
+
+    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
     
     //Camera
     auto focal_length = 1.0;
@@ -64,7 +67,7 @@ int main() {
             auto ray_direction = pixel_center - camera_center;
             ray r(camera_center, ray_direction);
 
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r, world);
             write_color(std::cout, pixel_color);
 
         }
